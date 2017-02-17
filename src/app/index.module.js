d339d248274cd1f29db10bb85a43d21b2b02a796
Drm1804 <drm-a@yandex.ru>
2017-02-17 13:30:22 +0500

    first commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('panel', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
